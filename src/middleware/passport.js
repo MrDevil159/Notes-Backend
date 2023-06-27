b4a0ab708296f14f1a0eade34648914e76ff468a
vishal <vishaliscoding27@gmail.com>
2023-06-27 17:51:19 +0530

    new

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const passportJWT = require("passport-jwt");
-const User = require("../model/User");
+const User = require("../model/user");
 
 const secretKey = process.env.SECRET_TOKEN;
 
@@ -8,20 +8,20 @@ const ExtractJWT = passportJWT.ExtractJwt;
 const JwtStrategy = passportJWT.Strategy;
 
 const jwtOptions = {
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: secretKey,
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: secretKey
 };
 
 const jwtStrategy = new JwtStrategy(jwtOptions, async (payload, done) => {
-  try {
-    const user = await User.findById(payload._id);
-    if (user) {
-      return done(null, user);
+    try {
+        const user = await User.findById(payload._id);
+        if (user) {
+            return done(null, user);
+        }
+        return done(null, false);
+    } catch (error) {
+        return done(error, false);
     }
-    return done(null, false);
-  } catch (error) {
-    return done(error, false);
-  }
 });
 
 passport.use(jwtStrategy);
