aecfa06628444cdfa2f6507b8d91c2634aa4a3d9
vishal <vishaliscoding27@gmail.com>
2023-06-27 17:29:00 +0530

    name change

--- src/model/user.js
+++ src/model/user.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports = mongoose.model("user", userSchema);
